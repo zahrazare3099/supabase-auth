ba13f5535b295d4c3907f3f0e334f7248d417217
ZahraZare <zahrazare3099@gmail.com>
2025-02-02 19:18:04 +0330

    fix bug in resposive page

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,5 +1,5 @@
 function FormLayout({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto mt-8 w-3/6 md:w-2/5 lg:w-2/6">{children}</div>;
+  return <div className="mx-auto mt-8 w-4/6 md:w-2/5 lg:w-2/6">{children}</div>;
 }
 
 type FormPropsType = {
